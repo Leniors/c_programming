18180f7025296bdca369343abce45e90dce5d005
Leniors <mokualeeroy@gmail.com>
2023-06-05 13:53:25 +0300

    math

--- math.c
+++ math.c
@@ -2,13 +2,34 @@
 
 int main(void)
 {
-	int i = 0;
+	double initial, step, stop, conv;
 
-	i = 0;
-	while (i < 10)
-	{
-		printf("%d", i);
-		i++;
+	do {
+		printf("Initial: ");
+		scanf("%lf", &initial);
+		if (initial < 0) {
+			printf("It must be greater than 0\n");
+		}
+	}while (initial < 0);
+	do {
+		printf("Step: ");
+		scanf("%lf", &step);
+		if (step < 0) {
+			printf("It must be greater than 0\n");
+		}
+	} while(step < 0);
+	do {
+		printf("Stop: ");
+		scanf("%lf", &stop);
+		if (stop < 0) {
+			printf("It must be greater than 0\n");
+		}
+	} while(stop < 0);
+
+	printf("Meters   Feet\n");
+	printf("******   ****\n");
+	for (conv = initial; conv <= stop; conv += step) {
+		printf("%6.2lf%7.2lf\n", conv, conv * 3.28084);
 	}
 	printf("\n");
 	return 0;
